c5b9d5c1a1759c5c5e9538386b42e80502d7aa39
guisoacelga <guiso.acelga@gmail.com>
2021-03-11 14:53:38 +0100

    add FlatArray.java

---
+++ src/FlatArray.java
@@ -0,0 +1,12 @@
+public class FlatArray {
+    public static void main(String[] args) {
+        System.out.println("Hello! I am going to turn a 2d array into a 1d array!!*****");
+
+        int [] [] numbers = {{0,1,9}, {6,3,2}};
+
+    }
+
+    public static int [] getFlatArray(int [] [] numbers) {
+
+    }
+}
